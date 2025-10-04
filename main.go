a0bfc4a91695d4c7cb7059eff20bab4d0196d8de
Maxence (Coddyum) <coddyumpro@gmail.com>
2025-10-04 17:24:31 +0200

    create a fileServers to add a index.html

--- main.go
+++ main.go
@@ -2,13 +2,17 @@ package main
 
 import "net/http"
 
-type Server struct {
-	Addr    string
-	Handler http.ServeMux
-}
+// type Server struct {
+// 	Addr    string
+// 	Handler http.ServeMux
+// }
 
 func main() {
 	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("."))
+
+	mux.Handle("/", fileServer)
 
 	http.ListenAndServe(":8080", mux)
 }
