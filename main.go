6d3d4e3b318a92adb4417d8b5257d892acbb56ae
Maxence (Coddyum) <coddyumpro@gmail.com>
2025-10-05 20:16:13 +0200

    finish the Servers section and init the Routing Section

--- main.go
+++ main.go
@@ -1,18 +1,63 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+)
 
-// type Server struct {
-// 	Addr    string
-// 	Handler http.ServeMux
-// }
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+// Incremente le hits x qu'un user var sur la main page (/app)
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+// Affiche le count du nombre de hits
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	count := cfg.fileserverHits.Load()
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Write([]byte(fmt.Sprintf("Hits: %d", count)))
+}
+
+// Reset le compteur de visite
+func (cfg *apiConfig) resetMetricHandler(w http.ResponseWriter, r *http.Request) {
+	count := cfg.fileserverHits.Load()
+	cfg.fileserverHits.CompareAndSwap(count, 0)
+}
+
+// Affiche le status de notre app
+func readlinessHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
 
 func main() {
 	mux := http.NewServeMux()
+	apiCfg := &apiConfig{}
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
 
 	fileServer := http.FileServer(http.Dir("."))
 
-	mux.Handle("/", fileServer)
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", fileServer)))
+	mux.Handle("/assets", fileServer)
 
-	http.ListenAndServe(":8080", mux)
+	// Metric
+	mux.HandleFunc("/metrics", apiCfg.metricsHandler)
+	mux.HandleFunc("/reset", apiCfg.resetMetricHandler)
+
+	// Status off app health
+	mux.HandleFunc("/healthz", readlinessHandler)
+
+	srv.ListenAndServe()
 }
